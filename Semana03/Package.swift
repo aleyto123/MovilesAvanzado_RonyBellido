2bc0b5c9bd95ea76ace4dbe6b26b2a4bda41ce88
aleyto123 <rony.bellido@tecsup.edu.pe>
2026-09-08 17:00:22 -0500

    Corregir sintaxis en Package.swift para Ejercicio01

--- Semana03/Package.swift
+++ Semana03/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "Ejercicio01",
-            path : "Sources/Ejercicio01"
+            path : "Sources/Ejercicio01",
         ),
     ],
 )
