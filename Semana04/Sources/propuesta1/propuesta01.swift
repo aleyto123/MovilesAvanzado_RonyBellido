f3c01320976dadebe8056cf9ed362e9f9ec2ff90
Rony Bellido <TU_CORREO_DE_GITHUB>
2026-09-15 12:26:47 -0500

    feat: crear clase Alumno y gestionar cursos

--- Semana04/Sources/propuesta1/propuesta01.swift
+++ Semana04/Sources/propuesta1/propuesta01.swift
@@ -8,3 +8,23 @@ struct Curso {
     let precio: Double
     let cantidad: Int
 }
+
+// Representa el alumno y sus cursos.
+class Alumno {
+    let nombre: String
+    let dni: String
+    let esAlumnoTecsup: Bool
+    var cursos: [Curso]
+
+    init(nombre: String, dni: String, esAlumnoTecsup: Bool) {
+        self.nombre = nombre
+        self.dni = dni
+        self.esAlumnoTecsup = esAlumnoTecsup
+        self.cursos = []
+    }
+
+    // Agrega un curso a la lista.
+    func agregarCurso(curso: Curso) {
+        cursos.append(curso)
+    }
+}
