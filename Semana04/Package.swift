bde1f2381e34aceb068def69e97621c69d612943
Rony Bellido <TU_CORREO_DE_GITHUB>
2026-09-15 11:57:38 -0500

    arreglo de errores en el package

--- Semana04/Package.swift
+++ Semana04/Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Semana04",
-            path: "Sources/Semana04"
+            name: "caso 2 parte b con ia",
+            path: "Sources/Caso 2 parte b con ia"
         ),
     ],
 )
